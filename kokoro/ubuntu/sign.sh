de27e1ae12df1caedbc081d5771004000aa9b344
Elliotte Rusty Harold <elharo@google.com>
2016-10-14 09:51:46 -0400

    sign plugins directpry

--- kokoro/ubuntu/sign.sh
+++ kokoro/ubuntu/sign.sh
@@ -14,3 +14,11 @@ mkdir -p signed && chmod 777 signed
 /escalated_sign/escalated_sign.py -j /escalated_sign_jobs -t signjar \
  $KOKORO_GFILE_DIR/content.jar \
  $KOKORO_GFILE_DIR/signed/content.jar
+ 
+FILES=$KOKORO_GFILE_DIR/plugins
+for f in $FILES
+do
+  echo "Processing $f file..."
+  # take action on each file. $f store current file name
+  # cat $f
+done
